f209ee0c37a02be347f2b07f87f1dea07b1fb1bc
James Roberts <jrwroberts1976@gmail.com>
2024-12-02 19:54:07 +0000

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+/**
+ *  *  * @jest-environment jsdom
+ *   *   */
+
+test('use jsdom in this test file', () => {
+	          const element = document.createElement('div');
+	          expect(element).not.toBeNull();
+});
+
+test('use jsdom in this test file', () => {
+	                  const element = document.createElement('div');
+	                  expect(element).not.toBeNull();
+});
